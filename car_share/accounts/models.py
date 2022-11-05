3ae6180068b4bdc42e9f00c586bdb91ec3fdae9d
Danisa-R <contactdanisa@gmail.com>
2022-11-05 13:10:58 -0400

    add moddels for user and review:

--- car_share/accounts/models.py
+++ car_share/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Account(models.Model):
+    user = models.ForeignKey(User, max_length=40)
+    last_name = models.TextField(max_length=40)
+    user_name = models.TextField(max_length=40)
+    password = models.TextField(max_length=40)
+
+
+
--- car_share/reviews/models.py
+++ car_share/reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+    score = models.IntegerField()
+    reviewer = models.TextField(max_size=40)
+    reviewee = models.TextField(max_size=40)
+    review_text = models.TextField(max_size=40)
+
+
+"""Review:
+    score
+    reviewer
+    reviewee
+    text of review
+"""
+
+# TODO: add and average out sum for the number of reviews
