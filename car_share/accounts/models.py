c824bf9b555275d96dfb279308594bbc736a71a6
Danisa-R <contactdanisa@gmail.com>
2022-11-05 15:23:07 -0400

    add html pages for review page and account profile

--- car_share/accounts/models.py
+++ car_share/accounts/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Account(models.Model):
-    user = models.ForeignKey(User, max_length=40)
+    user = models.ForeignKey(User, max_length=40) 
