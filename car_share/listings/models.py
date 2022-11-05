715756ed9beb73c692d4b3b4e47528e83a09f7ac
Danisa-R <contactdanisa@gmail.com>
2022-11-05 15:41:22 -0400

    Adding listing fields in models

--- car_share/listings/models.py
+++ car_share/listings/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Listings(models.Model):
-    # owner
-    # car_type
-    # gas_percent
-    # mileage
-    # make_model
-    # renter
+    location=models.TextField(max_length=40)
+    owner = models.TextField(max_length=40)
+    car_type = models.TextField(max_length=40)
+    gas_percent=models.IntegerField(max_length=40)
+    mileage=models.IntegerField(max_length=40)
+    make_model=models.TextField(max_length=40)
+    renter=models.TextField(max_length=40)
