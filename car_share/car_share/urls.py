1c1256f4f221c0542356bb9556f856ec82cb04aa
Danisa-R <contactdanisa@gmail.com>
2022-11-06 00:54:13 -0400

    add reviews, listings and accounts url endpoint to main urls.py in car_share

--- car_share/car_share/urls.py
+++ car_share/car_share/urls.py
@@ -17,6 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # reviews
+    path('reviews/', include('reviews.urls')),
+   
+    # listings
+    path('listings/', include('listings.urls')),
+    
+    # accounts
+    path('accounts/', include('accounts.urls')),
+
 ]
